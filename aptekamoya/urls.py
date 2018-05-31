ff3b9900f3e0e36b14610ca58c4dfe9c8e993630
Vsevolod Alokhin <vsevolod.alokhin@qvantel.com>
2018-05-31 21:56:50 +0300

    add authentication

--- aptekamoya/urls.py
+++ aptekamoya/urls.py
@@ -17,16 +17,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
-
-
-from django.contrib.auth import views
+from aptekamoya import views as core_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('landing.urls')),
-    url(r'^', include('products.urls')),
-    url(r'^', include('orders.urls')),
-] \
+                  url('^', include('django.contrib.auth.urls')),
+                  url(r'^signup/$', core_views.signup, name='signup'),
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^', include('landing.urls')),
+                  url(r'^', include('products.urls')),
+                  url(r'^', include('orders.urls')),
+              ] \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- aptekamoya/views.py
+++ aptekamoya/views.py
@@ -5,9 +5,26 @@ from django.views.generic.edit import FormView
 
 from products.models import *
 
+from aptekamoya.forms import SignUpForm
+
 
 def home(request):
     products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
     products_images_tablets = products_images.filter(product__category__id=1)
     products_images_virus = products_images.filter(product__category__id=2)
     return render(request, 'landing/home.html', locals())
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
